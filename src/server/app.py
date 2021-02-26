0b94d2161aa2777b217c3190716e302f12de390f
Pedro Lopes <pedrolopes09.pv@gmail.com>
2021-02-26 20:42:27 -0300

    feat: updating cors

--- src/server/app.py
+++ src/server/app.py
@@ -12,7 +12,7 @@ app.include_router(AuthRouter, tags=["Authentication"], prefix="/auth")
 origins = [
     "http://localhost",
     "http://localhost:4201",
-    "https://pedlop.github.io/bank-web/",
+    "https://pedlop.github.io",
 ]
 app.add_middleware(
     CORSMiddleware,
